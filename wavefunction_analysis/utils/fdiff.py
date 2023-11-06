a9ba9510157035e491fb1a5be576da1e9308d0eb
zheng oscer <zheng.pei.c@gmail.com>
2023-11-06 16:15:30 -0600

    finite-difference example

--- wavefunction_analysis/utils/fdiff.py
+++ wavefunction_analysis/utils/fdiff.py
@@ -58,20 +58,25 @@ if __name__ == '__main__':
     from wavefunction_analysis.utils.sec_mole import read_symbols_coords, write_mol_info_geometry, write_rem_info
     from wavefunction_analysis.utils.read_files import read_matrix, read_number
 
-    infile = 'h2o.in'
-    functional = 'hf'
-    basis = '3-21g'
+    infile = sys.argv[1]
+    jb = 'write'
+    if len(sys.argv) > 2: jb = sys.argv[2] # 'write' # cal
+
+    functional = read_number(infile, 'method ', n=1, ftype=str)
+    basis      = read_number(infile, 'basis ', n=1, ftype=str)
 
     norder, step_size = 2, 1e-4
     symbols, coords = read_symbols_coords(infile)
     natoms = len(symbols)
 
-    #jb = 'write'
-    jb = 'cal'
     suffixes = ['P ', 'angular '] # momentum, angular_momentum
+    fix2s    = ['', 'N']
     loop = 1 if jb == 'write' else len(suffixes)
-    contract = True # contract hessian with density
+    loop2 = 1 if jb == 'write' else len(fix2s)
+    contract = False #True # contract hessian with density
     nwidth = 5
+
+    p_momentum_3n_d1 = None
 
     for il in range(loop):
         suffix = suffixes[il]
@@ -80,15 +85,40 @@ if __name__ == '__main__':
             out0 = infile[:-3] + '.out'
             nbas = read_number(out0, 'NBas: ', n=1)
             den = read_matrix(out0, nbas, nbas, 'scf density matrix arma', nwidth=-1, nskip=2)
+            print('den-den.T:', np.linalg.norm(den-den.T))
 
-            momentum_d1 = read_matrix(out0, nbas*nbas, natoms*3*natoms*3, suffix+'momentum derivative 3N arma', nwidth=-1, nskip=2)
-            momentum_d1 = np.reshape(momentum_d1.T, (natoms, 3, natoms, 3, nbas, nbas))
-            if suffix == 'angular':
-                momentum_fd = np.einsum('ixjy...->xjy...', momentum_fd)
-            print_matrix(suffix+'d1:', momentum_d1, nwidth, 1, trans=True)
+            momentum_3 = read_matrix(out0, nbas*nbas, 3, suffix+'momentum 3 arma', nwidth=-1, nskip=2).T.reshape(3, nbas, nbas)
+            momentum_3n = read_matrix(out0, nbas*nbas, natoms*3, suffix+'momentum 3N arma', nwidth=-1, nskip=2).T.reshape(natoms, 3, nbas, nbas)
+            print(suffix+'momentum difference:', np.linalg.norm(momentum_3 - np.einsum('nx...->x...', momentum_3n)))
+            if suffix == 'angular ':
+                momentum_ref = read_matrix(out0, nbas*nbas, 3, 'angular momentum ref', nwidth=-1, nskip=2).T.reshape(3, nbas, nbas)
+                m_diff = momentum_3 - momentum_ref
+                print('angular difference ref:', np.linalg.norm(m_diff))
+            if suffix == 'P ':
+                momentum_ref = read_matrix(out0, nbas*nbas, natoms*3, 'SRx momentum 3N ref', nwidth=-1, nskip=2).T.reshape(natoms, 3, nbas, nbas)
+                m_diff = momentum_3n - momentum_ref
+                print('P difference ref:', np.linalg.norm(m_diff))
+            #    print_matrix(suffix+'momentum 3:', momentum_3, nwidth, nind=1)
+            #    #print_matrix(suffix+'momentum_3n:', momentum_3n, nwidth, 1)
+
+            momentum_3_d1 = read_matrix(out0, nbas*nbas, natoms*3*3, suffix+'momentum derivative 3 arma', nwidth=-1, nskip=2).T.reshape(3, natoms, 3, nbas, nbas)
+            momentum_3n_d1 = read_matrix(out0, nbas*nbas, natoms*3*natoms*3, suffix+'momentum derivative 3N arma', nwidth=-1, nskip=2).T.reshape(natoms, 3, natoms, 3, nbas, nbas)
+            print(suffix+'momentum derivative difference:', np.linalg.norm(momentum_3_d1 - np.einsum('nx...->x...', momentum_3n_d1)))
+            #if suffix == 'P ':
+            #    p_momentum_3n_d1 = np.copy(momentum_3n_d1)
+            #    #print_matrix(suffix+'momentum 3_d1:', momentum_3_d1, nwidth, nind=1)
+            #    print_matrix(suffix+'momentum_3n_d1:', momentum_3n_d1, nwidth, 1)
+
+            if contract:
+                momentum_3_d2 = read_matrix(out0, natoms*3*natoms*3, 3, suffix+'momentum derivative 2 3 arma', nwidth=-1, nskip=2).T.reshape(3, natoms*3, natoms*3)
+                momentum_3n_d2 = read_matrix(out0, natoms*3*natoms*3, natoms*3, suffix+'momentum derivative 2 3N arma', nwidth=-1, nskip=2).T.reshape(natoms, 3, natoms*3, natoms*3)
+            else:
+                momentum_3_d2 = read_matrix(out0, nbas*nbas*natoms*3*natoms*3, 3, suffix+'momentum derivative 2 3 arma', nwidth=-1, nskip=2).T.reshape(3, natoms*3, natoms*3, nbas, nbas)
+                momentum_3n_d2 = read_matrix(out0, nbas*nbas*natoms*3*natoms*3, natoms*3, suffix+'momentum derivative 2 3N arma', nwidth=-1, nskip=2).T.reshape(natoms, 3, natoms*3, natoms*3, nbas, nbas)
+            print(suffix+'momentum derivative 2 difference:', np.linalg.norm(momentum_3_d2 - np.einsum('nx...->x...', momentum_3n_d2)))
 
 
-        momentum_fd, momentum_fd2 = [], []
+        momentum_3_fd, momentum_3n_fd, momentum_3_fd2, momentum_3n_fd2 = [], [], [], []
         for n in range(natoms):
             for x in range(3):
                 fd = fdiff(norder, step_size)
@@ -102,36 +132,92 @@ if __name__ == '__main__':
                         write_rem_info(newfile, functional, basis)
 
                 elif jb == 'cal':
-                    momentum, momentum2 = [], []
+                    momentum, momentum2, momentum3, momentum4 = [], [], [], []
                     for d in range(norder*2):
                         newfile = infile[:-3]+'_'+str(n+1)+'_'+str(x+1)+'_'+str(d+1)+ '.out'
-                        m = read_matrix(newfile, nbas*nbas, natoms*3, suffix+'momentum 3N arma', nwidth=-1, nskip=2)
-                        m2 = read_matrix(newfile, nbas*nbas, natoms*3*natoms*3, suffix+'momentum derivative 3N arma', nwidth=-1, nskip=2)
+                        m = read_matrix(newfile, nbas*nbas, 3, suffix+'momentum 3 arma', nwidth=-1, nskip=2)
+                        m2 = read_matrix(newfile, nbas*nbas, 3*natoms*3, suffix+'momentum derivative 3 arma', nwidth=-1, nskip=2)
+                        m3 = read_matrix(newfile, nbas*nbas, natoms*3, suffix+'momentum 3N arma', nwidth=-1, nskip=2)
+                        m4 = read_matrix(newfile, nbas*nbas, natoms*3*natoms*3, suffix+'momentum derivative 3N arma', nwidth=-1, nskip=2)
                         momentum.append(m.T)
                         momentum2.append(m2.T)
+                        momentum3.append(m3.T)
+                        momentum4.append(m4.T)
                     momentum = fd.compute_fdiff(np.array(momentum), 1./BOHR)
                     momentum2 = fd.compute_fdiff(np.array(momentum2), 1./BOHR)
-                    momentum_fd.append(momentum)
-                    momentum_fd2.append(momentum2)
+                    momentum3 = fd.compute_fdiff(np.array(momentum3), 1./BOHR)
+                    momentum4 = fd.compute_fdiff(np.array(momentum4), 1./BOHR)
+                    momentum_3_fd.append(momentum)
+                    momentum_3_fd2.append(momentum2)
+                    momentum_3n_fd.append(momentum3)
+                    momentum_3n_fd2.append(momentum4)
 
 
         if jb == 'cal':
-            momentum_fd = np.reshape(momentum_fd, (natoms, 3, natoms, 3, nbas, nbas))
-            momentum_fd = np.einsum('ixjy...->jyix...', momentum_fd)
-            if suffix == 'angular':
-                momentum_fd = np.einsum('ixjy...->xjy...', momentum_fd)
-            print_matrix(suffix+'fd:', momentum_fd, nwidth, 1, trans=True)
-            print(suffix+'difference:', np.linalg.norm(momentum_fd+momentum_d1))
+            momentum_3_fd = np.reshape(momentum_3_fd, (natoms, 3, 3, nbas, nbas))
+            momentum_3_fd = np.einsum('ixy...->yix...', momentum_3_fd)
+            m_diff = momentum_3_fd-momentum_3_d1
+            print(suffix+'3 derivative difference:', np.linalg.norm(m_diff))
+            #if suffix == 'angular ':
+            #    print_matrix(suffix+'3 derivative difference:', m_diff, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3_d1:', momentum_3_d1, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3_fd:', momentum_3_fd, nwidth, 1)
+            momentum_3n_fd = np.reshape(momentum_3n_fd, (natoms, 3, natoms, 3, nbas, nbas))
+            momentum_3n_fd = np.einsum('ixjy...->jyix...', momentum_3n_fd)
+            m_diff = momentum_3n_fd-momentum_3n_d1
+            print(suffix+'3N derivative difference:', np.linalg.norm(m_diff))
+            #if suffix == 'angular ':
+            #    print_matrix(suffix+'3N derivative difference:', m_diff, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3n_d1:', momentum_3n_d1, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3n_fd:', momentum_3n_fd, nwidth, 1)
 
+            momentum_3_fd2 = np.reshape(momentum_3_fd2, (natoms*3, 3, natoms*3, nbas, nbas))
+            momentum_3n_fd2 = np.reshape(momentum_3n_fd2, (natoms*3, natoms*3, natoms*3, nbas, nbas))
             if contract:
-                momentum_fd2 = np.reshape(momentum_fd2, (natoms*3, natoms*3, natoms*3, nbas, nbas))
-                momentum_fd2 = np.einsum('ijkpq,pq->jki', momentum_fd2, den)
-                print_matrix(suffix+'fd2:', momentum_fd2, nwidth, 1)
+                momentum_3_fd2 = np.einsum('ijkpq,pq->jki', momentum_3_fd2, den)
+                momentum_3n_fd2 = np.einsum('ijkpq,pq->jki', momentum_3n_fd2, den)
+                momentum_3n_d2 = np.reshape(momentum_3n_d2, (natoms*3, natoms*3, natoms*3))
+                print_matrix(suffix+' fd2:', momentum_3n_fd2, nwidth, 1)
+                print_matrix(suffix+' d2:', momentum_3n_d2, nwidth, 1)
             else:
-                momentum_fd2 = np.reshape(momentum_fd2, (natoms*3, natoms*3, natoms*3, nbas, nbas))
-                momentum_fd2 = np.einsum('ijkpq->jkipq', momentum_fd2)
-                for i in range(natoms*3):
-                    for j in range(natoms*3):
-                        for k in range(natoms*3):
-                            print('i:', i+1, 'j:', j+1, 'k:', k+1, end=' ')
-                            print_matrix(suffix+'momentum derivative 2 3N:', momentum_fd2[i,j,k].T, nwidth, 1, trans=True)
+                momentum_3_fd2 = np.einsum('ijkpq->jkipq', momentum_3_fd2)
+                momentum_3n_fd2 = np.einsum('ijkpq->jkipq', momentum_3n_fd2)
+                momentum_3n_d2 = np.reshape(momentum_3n_d2, (natoms*3, natoms*3, natoms*3, nbas, nbas))
+        
+            m_diff = momentum_3_fd2 - momentum_3_d2
+            print(suffix+'3 derivative 2 difference:', np.linalg.norm(m_diff))
+            if suffix == 'P ':
+                print_matrix(suffix+'3 derivative 2 difference:', m_diff, nwidth, 1)
+                print_matrix(suffix+'momentum_3_d2', momentum_3_d2, nwidth, 1)
+                print_matrix(suffix+'momentum_3_fd2', momentum_3_fd2, nwidth, 1)
+
+                #momentum_3_d2 = np.reshape(momentum_3_d2, (3, natoms, 3, natoms, 3, nbas, nbas))
+                #p_momentum_3n_d1 = np.einsum('ixjypq->ixjyqp', p_momentum_3n_d1) - p_momentum_3n_d1
+                #for x in range(3):
+                #    y, z = (x+1) % 3, (x+2) % 3
+                #    momentum_3_d2[x,:,x,:,y] += p_momentum_3n_d1[:,z,:,x]
+                #    momentum_3_d2[x,:,x,:,z] -= p_momentum_3n_d1[:,y,:,x]
+
+                #    momentum_3_d2[x,:,y,:,x] += p_momentum_3n_d1[:,z,:,x]
+                #    momentum_3_d2[x,:,z,:,x] -= p_momentum_3n_d1[:,y,:,x]
+
+                #    momentum_3_d2[x,:,y,:,y] += 2.*p_momentum_3n_d1[:,z,:,y]
+                #    momentum_3_d2[x,:,z,:,z] -= 2.*p_momentum_3n_d1[:,y,:,z]
+
+                #    momentum_3_d2[x,:,y,:,z] += p_momentum_3n_d1[:,z,:,z]
+                #    momentum_3_d2[x,:,y,:,z] -= p_momentum_3n_d1[:,y,:,y]
+                #    
+                #    momentum_3_d2[x,:,z,:,y] += p_momentum_3n_d1[:,z,:,z]
+                #    momentum_3_d2[x,:,z,:,y] -= p_momentum_3n_d1[:,y,:,y]
+                #momentum_3_d2 = np.reshape(momentum_3_d2, (3, natoms*3, natoms*3, nbas, nbas))
+                #print_matrix(suffix+'momentum_3_d2 correct', momentum_3_d2, nwidth, 1)
+                #m_diff = momentum_3_fd2 - momentum_3_d2
+                #print_matrix(suffix+'3 derivative 2 difference correct:', m_diff, nwidth, 1)
+                #print(suffix+'3 derivative 2 difference correct:', np.linalg.norm(m_diff))
+
+            m_diff = momentum_3n_fd2 - momentum_3n_d2
+            print(suffix+'3N derivative 2 difference:', np.linalg.norm(m_diff))
+            #if suffix == 'P ':
+            #    print_matrix(suffix+'3N derivative 2 difference:', m_diff, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3n_d2', momentum_3n_d2, nwidth, 1)
+            #    print_matrix(suffix+'momentum_3n_fd2', momentum_3n_fd2, nwidth, 1)
