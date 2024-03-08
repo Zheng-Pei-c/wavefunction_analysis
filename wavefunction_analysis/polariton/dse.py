a2ba254c1e6c51d2a9f480bb8ecbea5aaccd155b
zheng <zheng.pei.c@gmail.com>
2024-03-07 20:54:44 -0600

    update polariton dse code before Hessian!

--- wavefunction_analysis/polariton/dse.py
+++ wavefunction_analysis/polariton/dse.py
@@ -17,7 +17,7 @@ def get_scaled_lambda(c_lambda, frequency):
 
 def get_nuclear_dipoles(mol, c_lambda, origin=None):
     """
-    lambda \cdot nuclear_dipole
+    lambda cdot nuclear_dipole
     """
     if origin is None:
         origin = np.zeros(3)
@@ -31,7 +31,8 @@ def get_nuclear_dipoles(mol, c_lambda, origin=None):
     return np.einsum('x,...x->...', nuc_dip, c_lambda)
 
 
-def get_multipole_matrix(mol, itype='dipole', c_lambda=None, origin=None):
+def get_multipole_matrix(mol, itype='dipole', dipole=None, quadrupole=None,
+                         c_lambda=None, origin=None):
     """
     c_lambda: (n_mode, 3) = coupling_strength * sqrt(2.*photon_frequency)
     """
@@ -40,24 +41,31 @@ def get_multipole_matrix(mol, itype='dipole', c_lambda=None, origin=None):
     if isinstance(c_lambda, list):
         c_lambda = np.array(c_lambda)
 
-    multipoles = {}
+    if itype == 'all':
+        if not isinstance(dipole, np.ndarray):
+            itype += '_dipole'
+        if not isinstance(quadrupole, np.ndarray):
+            itype += '_quadrupole'
+
     with mol.with_common_orig(origin):
         if 'dipole' in itype:
             dipole = mol.intor('int1e_r', comp=3, hermi=0)
-            if isinstance(c_lambda, np.ndarray):
-                dipole = np.einsum('xpq,...x->...pq', dipole, c_lambda)
-            multipoles['dipole'] = dipole
 
         if 'quadrupole' in itype:
             nao = mol.nao_nr()
             quadrupole = mol.intor('int1e_rr', comp=9, hermi=0).reshape(3,3,nao,nao)
-            if isinstance(c_lambda, np.ndarray):
-                c2 = np.einsum('...x,...y->...xy', c_lambda, c_lambda)
-                if c2.ndim == 3: # contract modes
-                    c2 = np.sum(c2, axis=0)
-                quadrupole = np.einsum('xypq,xy->pq', quadrupole, c2)
-            multipoles['quadrupole'] = quadrupole
 
+    if isinstance(c_lambda, np.ndarray):
+        if isinstance(dipole, np.ndarray):
+            dipole = np.einsum('xpq,...x->...pq', dipole, c_lambda)
+
+        if isinstance(quadrupole, np.ndarray):
+            c2 = np.einsum('...x,...y->...xy', c_lambda, c_lambda)
+            if c2.ndim == 3: # contract modes
+                c2 = np.sum(c2, axis=0)
+            quadrupole = np.einsum('xypq,xy->pq', quadrupole, c2)
+
+    multipoles = {'dipole': dipole, 'quadrupole': quadrupole}
     return multipoles
 
 
@@ -73,109 +81,41 @@ def get_energy_nuc_dip(nuc_dip):
     return energy
 
 
-def get_dse_2e(dipole, den, with_j=False): # c_lambda is included
+def get_dse_2e(dipole, dm, with_j=False, scale_k=.5): # c_lambda is included
+    # scale k by 1/2 for restricted orbital case by default
     if dipole.ndim == 2:
-        vk = np.einsum('pq,rs,...qs->...pr', dipole, dipole, den)
+        vk = np.einsum('pq,rs,...qs->...pr', dipole, dipole, dm)
         if with_j is False:
-            return vk
+            return vk*scale_k
         else:
-            vj = np.einsum('pq,rs,...rs->...pq', dipole, dipole, den)
-            return [vj, vk]
+            vj = np.einsum('pq,rs,...rs->...pq', dipole, dipole, dm)
+            return [vj, vk*scale_k]
     else: # contract modes
-        vk = np.einsum('ipq,irs,...qs->...pr', dipole, dipole, den)
+        vk = np.einsum('ipq,irs,...qs->...pr', dipole, dipole, dm)
         if with_j is False:
-            return vk
+            return vk*scale_k
         else:
-            vj = np.einsum('ipq,irs,...rs->...pq', dipole, dipole, den)
-            return [vj, vk]
+            vj = np.einsum('ipq,irs,...rs->...pq', dipole, dipole, dm)
+            return [vj, vk*scale_k]
 
 
-def get_dse_2e_xyz(dipole, den): # xyz without coupling
-    return np.einsum('xpq,yrs,...qs->...xypr', dipole, dipole, den)
-
-
-def cal_dse_gs(mol, den, c_lambda, dipole=None, quadrupole=None,
-               coherent_state=True):
-
-    itype = ''
-    if isinstance(dipole, np.ndarray):
-        dipole = np.einsum('xpq,...x->...pq', dipole, c_lambda)
-    else:
-        itype = itype + 'dipole'
-    if isinstance(quadrupole, np.ndarray):
-        c2 = np.einsum('...x,...y->...xy', c_lambda, c_lambda)
-        if c2.ndim == 3: # contract modes
-            c2 = np.sum(c2, axis=0)
-        quadrupole = np.einsum('xypq,xy->pq', quadrupole, c2)
-    else:
-        itype = itype + 'quadrupole'
-
-    if 'pole' in itype:
-        multipoles = get_multipole_matrix(mol, itype, c_lambda)
-        dipole = multipoles.get('dipole', dipole)
-        quadrupole = multipoles.get('quadrupole', quadrupole)
-
-    if den.ndim != 2 and den.ndim != 3:
-        # 2: restricted total density matrix
-        # 3: alpha and beta density matrices
-        raise ValueError('wrong density matrix')
-
-    if coherent_state:
-        vk = get_dse_2e(dipole, den, with_j=False)
-
-        dse = np.zeros(3)
-        dse[0] = .5* np.einsum('pq,...pq->', quadrupole, den) # need 1/2 for dse here
-        dse[1] = -.5* np.einsum('...pq,...pq->', vk, den) # need 1/2 for dse here
-        if den.ndim == 2:
-            dse[1] *= .5 # remove exchange for different spins
-
-    else:
-        nuc_dip = get_nuclear_dipoles(mol, c_lambda)
-        nuc_dip_fock = get_dse_elec_nuc(dipole, nuc_dip)
-        vj, vk = get_dse_2e(dipole, den, with_j=True)
-
-        dse = np.zeros(6)
-        dse[0] = .5* np.einsum('pq,...pq->', quadrupole, den) # need 1/2 for dse here
-        dse[1] = np.einsum('pq,...pq->', nuc_dip_fock, den)
-        dse[2] = .5* np.einsum('...pq,...pq->', vj, den) # need 1/2 for dse here
-        dse[3] = -.5* np.einsum('...pq,...pq->', vk, den) # need 1/2 for dse here
-        dse[4] = get_energy_nuc_dip(nuc_dip)
-        if den.ndim == 2:
-            dse[3] *= .5 # remove exchange for different spins
-
-    dse[-1] = np.sum(dse)
-    return dse
+def get_dse_2e_xyz(dipole, dm): # xyz without coupling
+    return np.einsum('xpq,yrs,...qs->...xypr', dipole, dipole, dm)
 
 
 
-from pyscf import dft
+from pyscf import lib, dft
+from pyscf.lib import logger
 from pyscf.dft.rks import RKS
 class polariton(RKS):
     """
     QED-RKS ground state, independent of photon frequency
     """
-    def get_multipole_matrix(self, c_lambda):
-        mol = self.mol
-        multipoles = get_multipole_matrix(mol, 'dipole_quadrupole', c_lambda)
+    def get_multipole_matrix(self, c_lambda, dipole=None, quadrupole=None):
+        multipoles = get_multipole_matrix(self.mol, 'all', dipole, quadrupole, c_lambda=c_lambda)
         self.c_lambda = c_lambda
         self.dipole = multipoles['dipole']
         self.quadrupole = multipoles['quadrupole']
-
-        self.get_exchange_factor()
-
-
-    def get_exchange_factor(self):
-        # the dse_2e term is added to exchange integral in fock
-        # so we need to correct the hyb factor for dse_2e
-        dft_type = 0
-        if self.xc != 'wb97xd':
-            dft_type = dft.xcfun.parse_xc(self.xc)
-        else:
-            dft_type = dft.libxc.parse_xc(self.xc)
-            omega, alpha, hyb = self._numint.rsh_and_hybrid_coeff(self.mf.xc)
-            dft_type[0][:] = [hyb, alpha, omega]
-        hyb, alpha, omega = dft_type[0]
-        self.hyb = hyb
 
 
 
@@ -185,45 +125,116 @@ class polariton_cs(polariton):
     """
     def get_hcore(self, mol=None):
         h = super().get_hcore(mol) # from RKS class
-        h += .5* self.quadrupole # need 1/2 for dse
+        hquad = .5* self.quadrupole # need 1/2 for dse
+        h += hquad
+        h = lib.tag_array(h, hquad=hquad)
         return h
 
 
-    def get_jk(self, mol=None, dm=None, hermi=1, with_j=True, with_k=True,
-               omega=None):
+    def get_veff(self, mol=None, dm=None, *args, **kwargs):
         if dm is None: dm = self.make_rdm1()
-        vj, vk = super().get_jk(mol, dm, hermi, with_j, with_k, omega)
-        vk += get_dse_2e(self.dipole, dm, with_j=False) / self.hyb # need 1/2 for dse
-        return vj, vk
+        vxc = super().get_veff(mol, dm, *args, **kwargs)
+        vdse_k = get_dse_2e(self.dipole, dm, with_j=False) # need 1/2 for dse
+        edse_k = -.5* np.einsum('...pq,...pq->', vdse_k, dm) # dse exchange energy
+
+        # old tags are destroyed after the number operations
+        ecoul, exc, vj, vk = vxc.ecoul, vxc.exc, vxc.vj, vxc.vk
+        vxc -= vdse_k
+        vxc = lib.tag_array(vxc, ecoul=ecoul, exc=exc, vj=vj, vk=vk, edse_k=edse_k, vdse_k=vdse_k)
+        return vxc
+
+
+    def energy_elec(self, dm=None, h1e=None, vhf=None):
+        if dm is None: dm = self.make_rdm1()
+        if h1e is None: h1e = self.get_hcore()
+        if vhf is None or getattr(vhf, 'edse_k', None) is None:
+            vhf = self.get_veff(self.mol, dm)
+
+        e_tot, e2 = super().energy_elec(dm, h1e, vhf)
+
+        equad = np.einsum('pq,...pq->', h1e.hquad, dm)
+        self.scf_summary['equad'] = equad.real
+        self.scf_summary['edse_k'] = vhf.edse_k.real
+        logger.debug(self, 'Quadrupole Energy = %s  DSE-K Energy = %s', equad, vhf.edse_k)
+        return e_tot, e2
+
+
+    def get_coupling_energy(self, dm=None, unit='ev'):
+        if isinstance(dm, np.ndarray):
+            self.energy_tot(dm=dm)
+
+        e = self.scf_summary
+        e = [e['equad'], e['edse_k']]
+        e.append(e[0] + e[1])
+        return convert_units(np.array(e), 'hartree', unit)
 
 
 
 class polariton_ns(polariton):
     """
-    in photon number states
+    in photon number states, not recommended!
     """
     def get_hcore(self, mol=None):
+        if mol is None: mol = self.mol
         h = super().get_hcore(mol) # from RKS class
-        h += .5* self.quadrupole # need 1/2 for dse
-        self.nuc_dip = get_nuclear_dipoles(self.mol, self.c_lambda)
-        h += get_dse_elec_nuc(self.dipole, self.nuc_dip)
+        hquad = .5* self.quadrupole # need 1/2 for dse
+        self.nuc_dip = get_nuclear_dipoles(mol, self.c_lambda)
+        hdipe = get_dse_elec_nuc(self.dipole, self.nuc_dip)
+
+        h += (hquad + hdipe)
+        h = lib.tag_array(h, hquad=hquad, hdipe=hdipe)
         return h
 
 
-    def get_jk(self, mol=None, dm=None, hermi=1, with_j=True, with_k=True,
-               omega=None):
+    def get_veff(self, mol=None, dm=None, *args, **kwargs):
         if dm is None: dm = self.make_rdm1()
-        vj, vk = super().get_jk(mol, dm, hermi, with_j, with_k, omega)
-        dse_j, dse_k = get_dse_2e(self.dipole, dm, with_j=True)
-        vj += dse_j
-        vk += dse_k / self.hyb
-        return vj, vk
+        vxc = super().get_veff(mol, dm, *args, **kwargs)
+        vdse_j, vdse_k = get_dse_2e(self.dipole, dm, with_j=True)
+        edse_j = .5* np.einsum('...pq,...pq->', vdse_j, dm) # need 1/2 for dse here
+        edse_k = -.5* np.einsum('...pq,...pq->', vdse_k, dm) # need 1/2 for dse here
+
+        ecoul, exc, vj, vk = vxc.ecoul, vxc.exc, vxc.vj, vxc.vk
+        vxc += (vdse_j - vdse_k)
+        vxc = lib.tag_array(vxc, ecoul=ecoul, exc=exc, vj=vj, vk=vk, edse_j=edse_j, edse_k=edse_k, vdse_j=vdse_j, vdse_k=vdse_k)
+        return vxc
 
 
-    def energy_tot(self, dm=None, h1e=None, vhf=None):
-        e_tot = super().energy_tot(dm, h1e, vhf)
-        e_tot += get_energy_nuc_dip(self.nuc_dip)
-        return e_tot
+    def energy_elec(self, dm=None, h1e=None, vhf=None):
+        if dm is None: dm = self.make_rdm1()
+        if h1e is None: h1e = self.get_hcore()
+        if vhf is None or getattr(vhf, 'edse_k', None) is None:
+            vhf = self.get_veff(self.mol, dm)
+
+        e_tot, e2 = super().energy_elec(dm, h1e, vhf)
+
+        equad = np.einsum('pq,...pq->', h1e.hquad, dm)
+        edipe = np.einsum('pq,...pq->', h1e.hdipe, dm)
+        self.scf_summary['equad'] = equad.real
+        self.scf_summary['edipe'] = edipe.real
+        self.scf_summary['edse_j'] = vhf.edse_j.real
+        self.scf_summary['edse_k'] = vhf.edse_k.real
+        logger.debug(self, 'Quadrupole Energy = %s  Nuclear-Electronic Dipole Energy = %s  DSE-J Energy = %s  DSE-K Energy = %s', equad, edipe, vhf.edse_j, vhf.edse_k)
+        return e_tot, e2
+
+
+    def energy_nuc(self):
+        enuc = super().energy_nuc()
+        edipn = get_energy_nuc_dip(self.nuc_dip)
+
+        self.scf_summary['edipn'] = edipn
+        logger.debug(self, 'Nuclear Dipole Energy = %s', edipn)
+        return (enuc+edipn)
+
+
+    def get_coupling_energy(self, dm=None, unit='ev'):
+        if isinstance(dm, np.ndarray):
+            self.get_hcore() # get nuc_dip for the energy
+            self.energy_tot(dm=dm)
+
+        e = self.scf_summary
+        e = [e['equad'], e['edipe'], e['edse_j'], e['edse_k'], e['edipn']]
+        e.append(np.sum(e))
+        return convert_units(np.array(e), 'hartree', unit)
 
 
 
@@ -275,8 +286,8 @@ if __name__ == '__main__':
     e_tot0 = mf.kernel()
     nocc = mol.nelectron // 2
 
-    den = mf.make_rdm1()
-    multipoles = get_multipole_matrix(mol, 'dipole_quadrupole')
+    dm = mf.make_rdm1()
+    multipoles = get_multipole_matrix(mol, itype='dipole_quadrupole')
     dipole, quadrupole = multipoles['dipole'], multipoles['quadrupole']
 
     frequency = 0.42978 # gs doesn't depend on frequency
@@ -289,8 +300,6 @@ if __name__ == '__main__':
         for x in range(2, 3):
             coupling = np.zeros(3)
             coupling[x] = c*1e-2
-            e0 = cal_dse_gs(mol, den, coupling, dipole, quadrupole, coherent_state)
-            e0 = convert_units(e0, 'hartree', 'ev')
 
             if coherent_state:
                 mf1 = polariton_cs(mol) # in coherent state
@@ -301,12 +310,11 @@ if __name__ == '__main__':
             mf1.xc = functional
             mf1.grids.prune = True
             mf1.get_multipole_matrix(coupling)
-            e_tot = mf1.kernel()#(dm0=den)
 
-            e1 = cal_dse_gs(mol, mf1.make_rdm1(), coupling, dipole, quadrupole, coherent_state)
-            e1 = convert_units(e1, 'hartree', 'ev')
+            e0 = mf1.get_coupling_energy(dm=dm)
+            e_tot = mf1.kernel()#(dm0=dm)
+            e1 = mf1.get_coupling_energy()
 
             e_tot = np.array([e_tot0, e_tot])
             e_tot = convert_units(e_tot, 'hartree', 'ev')
-
             print_qed_dse_energy(coupling[x], e0, e1, e_tot)
