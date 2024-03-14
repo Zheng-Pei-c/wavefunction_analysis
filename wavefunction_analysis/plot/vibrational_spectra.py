fix g1 derivative and change dipole_d1 index order

wavefunction_analysis/plot/vibrational_spectra.py
@@ -89,16 +89,16 @@ def get_dipole_dev(mf, hessobj, origin=None):
     mo1 = lib.chkfile.load(hessobj.chkfile, 'scf_mo1')
     mo1 = {int(k): mo1[k] for k in mo1}
 
-    g1 = np.zeros((natm, 3, 3))
+    g1 = np.zeros((natm, 3, 3)) # first 3 is derivative direction
     aoslices = mol.aoslice_by_atom()
     for k, ia in enumerate(atmlst):
-        p0, p1 = aoslices[ia,2:]
+        p0, p1 = aoslices[ia, 2:]
         dm1 = np.einsum('xpi,qi->xpq', mo1[ia], mocc)
 
-        g1[k] += np.einsum('ypq,xqp->xy', dipole, dm1)*2.
+        g1[k] += np.einsum('ypq,xqp->xy', dipole, dm1)*2. # 2 for dm1
         g1[k] -= np.einsum('xypq,qp->xy', dipole_d1[:,:,p0:p1], dm[:,p0:p1]) # dipole_d1 has extra minus
 
-    g1 *= -2. # electron dipole is negative
+    g1 *= -2. # electron dipole is negative # 2 for ket
 
     z = mol.atom_charges()
     for i in range(natm): # nuclear dipole derivative

wavefunction_analysis/polariton/qed_ks_grad.py
@@ -21,13 +21,13 @@ def finite_difference(mf, norder=2, step_size=1e-4, ideriv=2, extra=False):
     natoms = mol.natm
     coords = mol.atom_coords()
 
-    fd_e, fd_mo, fd_dip = [], [], []
+    fd_e, fd_mo, fd_dip, fd_g = [], [], [], []
     for n in range(natoms):
         for x in range(3):
             fd = fdiff(norder, step_size)
             coords_new = fd.get_x(coords, [n, x])
 
-            gs_dipole_d1, mo1, g1 = [], [], []
+            g1, mo1, gs_dipole1, transp = [], [], [], []
             for k in range(coords_new.shape[0]):
                 mol_new = mol.set_geom_(coords_new[k], inplace=False, unit='bohr')
                 mf1 = polariton_cs(mol_new) # in coherent state
@@ -46,21 +46,23 @@ def finite_difference(mf, norder=2, step_size=1e-4, ideriv=2, extra=False):
                 if extra:
                     mo1.append(change_matrix_phase_c(mo, mf1.mo_coeff))
                     #print_matrix('mo '+str(n)+' '+str(x)+' '+str(k)+':', mo1[-1], 5, 1)
-                    gs_dipole_d1.append(mf1.dip_moment(mol_new, unit='au'))
+                    gs_dipole1.append(mf1.dip_moment(mol_new, unit='au'))
+                    if isinstance(extra, float) or isinstance(extra, list) or isinstance(extra, np.ndarray): # extra is the frequency
+                        transp.append(np.einsum('...pq,qp,...->...', mf1.dipole, mf1.make_rdm1(), extra))
 
-            g1 = fd.compute_fdiff(np.array(g1))
-            fd_e.append(g1)
+            fd_e.append( fd.compute_fdiff(np.array(g1)))
 
             if extra:
-                mo1 = fd.compute_fdiff(np.array(mo1))
-                fd_mo.append(mo1)
-                gs_dipole_d1 = fd.compute_fdiff(np.array(gs_dipole_d1))
-                fd_dip.append(gs_dipole_d1)
+                fd_mo.append( fd.compute_fdiff(np.array(mo1)))
+                fd_dip.append( fd.compute_fdiff(np.array(gs_dipole1)))
+                if isinstance(extra, float) or isinstance(extra, list) or isinstance(extra, np.ndarray): # extra is the frequency
+                    fd_g.append( fd.compute_fdiff(np.array(transp)) )
 
-    return np.reshape(fd_e, (3*natoms, -1)), np.array(fd_mo), np.array(fd_dip)
+    return np.reshape(fd_e, (3*natoms, -1)), np.array(fd_mo), np.array(fd_dip), np.array(fd_g)
 
 
 def cal_multipole_matrix_fd(mol, dm=None, origin=None, norder=2, step_size=1e-4, ideriv=1):
+    # no coupling strength
     if origin is None:
         origin = np.zeros(3)
 
@@ -86,8 +88,12 @@ def cal_multipole_matrix_fd(mol, dm=None, origin=None, norder=2, step_size=1e-4,
                         ints1 = mol_new.intor('int1e_r', comp=3, hermi=0)
                         ints2 = mol_new.intor('int1e_rr', comp=9, hermi=0)
                         if combine:
-                            ints1 = np.einsum('xpq,...pq->x...', ints1, dm)
-                            ints2 = np.einsum('xpq,...pq->x...', ints2, dm).reshape(3,3)
+                            ints1 = np.einsum('xpq,...pq->...x', ints1, dm)
+                            ints2 = np.einsum('xpq,...pq->...x', ints2, dm)
+                            if dm.ndim == 2:
+                                ints2 = ints2.reshape(3,3)
+                            elif dm.ndim == 3:
+                                ints2 = ints2.reshape(-1,3,3)
                         else:
                             ints2 = ints2.reshape(3,3,nao,nao)
 
@@ -121,7 +127,7 @@ def cal_multipole_matrix_fd(mol, dm=None, origin=None, norder=2, step_size=1e-4,
     return dipole_d1, quadrupole_d1
 
 
-def cal_multipole_matrix_d1(mol, dm=None, origin=None):
+def cal_multipole_matrix_d1(mol, dm=None, origin=None): # no coupling strength
     if origin is None:
         origin = np.zeros(3)
 
@@ -177,26 +183,27 @@ def get_multipole_matrix_d1(mol, c_lambda, origin=None):
         quadrupole = mol.intor('int1e_irrp', comp=27, hermi=0).reshape(3,3,3,nao,nao)
 
     # these derivatives don't distinguish nuclei
-    dipole = - np.einsum('mxpq,...m->...xqp', dipole, c_lambda)
+    dipole = - np.einsum('mxpq,...m->xqp...', dipole, c_lambda) # move mode index to the last for convenience latter
     quadrupole = - np.einsum('mnxpq,mn->xqp', quadrupole, get_lambda2(c_lambda))
     return dipole, quadrupole
 
 
 def get_dse_2e(dipole, dipole_d1, dm, with_j=False, scale_k=.5): # c_lambda is included
     # scale k by 1/2 for restricted orbital case by default
+    # we moved the mode index for lambda-dipole derivative to the last
     if dm.ndim == 2:
-        vk = np.einsum('...xpq,...rs,qr->xps', dipole_d1, dipole, dm)
+        vk = np.einsum('xpq...,...rs,qr->xps', dipole_d1, dipole, dm)
         if with_j is False:
             return vk*scale_k
         else:
-            vj = np.einsum('...xpq,...rs,sr->xpq', dipole_d1, dipole, dm)
+            vj = np.einsum('xpq...,...rs,sr->xpq', dipole_d1, dipole, dm)
             return [vj, vk*scale_k]
     else: # multiple density matrices, ie uhf
-        vk = np.einsum('...xpq,...rs,iqr->ixps', dipole_d1, dipole, dm)
+        vk = np.einsum('xpq...,...rs,iqr->ixps', dipole_d1, dipole, dm)
         if with_j is False:
             return vk*scale_k
         else:
-            vj = np.einsum('...xpq,...rs,isr->ixpq', dipole_d1, dipole, dm)
+            vj = np.einsum('xpq...,...rs,isr->ixpq', dipole_d1, dipole, dm)
             return [vj, vk*scale_k]
 
 

wavefunction_analysis/polariton/qed_ks_hess.py
@@ -56,13 +56,24 @@ def resemble_deriv_on_atoms(mol, mat0):
     aoslices = mol.aoslice_by_atom()
     nao = mat0.shape[-1]
 
-    mat1 = np.zeros((mol.natm, 3, nao, nao))
-    for k, ia in enumerate(atmlst):
-        p0, p1 = aoslices[ia,2:]
-        mat1[k,:,p0:p1] += mat0[:,p0:p1]
-        mat1[k,:,:,p0:p1] += mat0[:,p0:p1].transpose(0,2,1)
+    if mat0.ndim == 3:
+        mat1 = np.zeros((mol.natm, 3, nao, nao))
+        for k, ia in enumerate(atmlst):
+            p0, p1 = aoslices[ia,2:]
+            mat1[k,:,p0:p1] += mat0[:,p0:p1]
+            mat1[k,:,:,p0:p1] += mat0[:,p0:p1].transpose(0,2,1)
 
-    return mat1.reshape(-1, nao, nao)
+        return mat1.reshape(-1, nao, nao)
+
+    elif mat0.ndim == 4:
+        nd = mat0.shape[-1] # number of photon mode
+        mat1 = np.zeros((mol.natm, 3, nao, nao, nd))
+        for k, ia in enumerate(atmlst):
+            p0, p1 = aoslices[ia,2:]
+            mat1[k,:,p0:p1] += mat0[:,p0:p1]
+            mat1[k,:,:,p0:p1] += mat0[:,p0:p1].transpose(0,2,1,3)
+
+        return mat1.reshape(-1, nao, nao, nd)
 
 
 def get_multipole_matrix_d2(mol, c_lambda, origin=None):
@@ -110,26 +121,27 @@ def get_dse_2e_a(dipole, dipole_d2, dm, with_j=False, scale_k=.5): # c_lambda is
     return [vj, vk*scale_k]
 
 
-def get_dse_2e_s(dipole_d1, dm, with_j=False, scale_k=.5): # c_lambda is included
-    # scale k by 1/2 for restricted orbital case by default
-    if dm.ndim == 2:
-        vk = np.einsum('...xrs,qr->...xqs', dipole_d1, dm)
-        vk = np.einsum('...xqs,...ysq->xy', vk, vk)
-        if with_j is False:
-            return vk*scale_k
-        else:
-            vj = np.einsum('...rs,sr->...x', dipole_d1, dm)
-            vj = np.einsum('...x,...y->xy', vj, vj)
-            return [vj, vk*scale_k]
-    else: # multiple density matrices, ie uhf
-        vk = np.einsum('...xrs,iqr->ixqs', dipole_d1, dm)
-        vk = np.einsum('...ixqs,...iysq->xy', vk, vk)
-        if with_j is False:
-            return vk*scale_k
-        else:
-            vj = np.einsum('...xrs,isr->i...x', dipole_d1, dm)
-            vj = np.einsum('i...x,i...y->xy', vj, vj)
-            return [vj, vk*scale_k]
+#def get_dse_2e_s(dipole_d1, dm, with_j=False, scale_k=.5): # c_lambda is included
+#    # scale k by 1/2 for restricted orbital case by default
+#    # the mode index for lambda-dipole_d1 is moved to the last
+#    if dm.ndim == 2:
+#        vk = np.einsum('xrs...,qr->...xqs', dipole_d1, dm)
+#        vk = np.einsum('...xqs,...ysq->xy', vk, vk)
+#        if with_j is False:
+#            return vk*scale_k
+#        else:
+#            vj = np.einsum('rs...,sr->...x', dipole_d1, dm)
+#            #vj = np.einsum('...x,...y->xy', vj, vj)
+#            return [vj, vk*scale_k]
+#    else: # multiple density matrices, ie uhf
+#        vk = np.einsum('xrs...,iqr->...ixqs', dipole_d1, dm)
+#        #vk = np.einsum('...ixqs,...iysq->xy', vk, vk)
+#        if with_j is False:
+#            return vk*scale_k
+#        else:
+#            vj = np.einsum('xrs...,isr->i...x', dipole_d1, dm)
+#            #vj = np.einsum('i...x,i...y->xy', vj, vj)
+#            return [vj, vk*scale_k]
 
 
 def partial_hess_elec(hessobj, mo_energy=None, mo_coeff=None, mo_occ=None,
@@ -149,42 +161,55 @@ def partial_hess_elec(hessobj, mo_energy=None, mo_coeff=None, mo_occ=None,
     de2 = rks_hess.partial_hess_elec(hessobj, mo_energy, mo_coeff, mo_occ,
                                      atmlst, max_memory, verbose)
 
+    # 2e-dse 1/2 is cancled by the derivative factor 2.
+
+    # these derivatives are distributed on atoms since they are from finite-difference
+    # TODO: get analytic derivatives and resemble them here
     dipole_d2, quadrupole_d2 = get_multipole_matrix_d2(mol, mf.c_lambda, hessobj.base.origin)
     vdse = .5* np.einsum('xypq,...qp->xy', quadrupole_d2, dm0)
-
     vdse -= get_dse_2e_a(mf.dipole, dipole_d2, dm0, with_j=False)
 
+    vdse = vdse.reshape(mol.natm, 3, mol.natm, 3).transpose(0,2,1,3)
+
     dipole_d1, _ = get_multipole_matrix_d1(mol, mf.c_lambda, mf.origin)
-    dipole_d1 = resemble_deriv_on_atoms(mol, dipole_d1)
-    vdse -= get_dse_2e_s(dipole_d1, dm0, with_j=False)
+    #dipole_d1 = resemble_deriv_on_atoms(mol, dipole_d1)
+    #vdse -= get_dse_2e_s(dipole_d1, dm0, with_j=False)
 
-    natoms = mol.natm
-    de2 += vdse.reshape(natoms, 3, natoms, 3).transpose(0,2,1,3)
+    aoslices = mol.aoslice_by_atom()
+    for i0, ia in enumerate(atmlst):
+        p0, p1 = aoslices[ia, 2:]
 
-    ## explicit derivatives of dse contribution
-    #for i0, ia in enumerate(atmlst):
-    #    shl0, shl1, p0, p1 = aoslices[ia]
-    #    shls_slice = (shl0, shl1) + (0, mol.nbas)*3
+        tmp1 = np.einsum('xrs...,qr->...xqs', dipole_d1[:,p0:p1], dm0[:,p0:p1]) # r is derivative index
+        tmp2 = np.einsum('xrs...,qr->...xqs', dipole_d1[:,p0:p1].transpose(0,2,1), dm0) # s is derivative index
+        for j0, ja in enumerate(atmlst):
+            q0, q1 = aoslices[ja, 2:]
+
+            tmp3 = np.einsum('xpq...,ps->...xsq', dipole_d1[:,q0:q1], dm0[q0:q1]) # r is derivative index
+            tmp4 = np.einsum('xpq...,ps->...xsq', dipole_d1[:,q0:q1], dm0[q0:q1,p0:p1]) # s is derivative index
+            vdse[i0,j0] -= np.einsum('...xqs,...ysq->xy', tmp1, tmp3)
+            vdse[i0,j0] -= np.einsum('...xqs,...ysq->xy', tmp2, tmp4)
+
+    de2 += vdse
 
     return de2
 
 
 def dse_fock_d1_s(dipole, dipole_d1, dm, with_j=False, scale_k=.5): # c_lambda is included
     # scale k by 1/2 for restricted orbital case by default
-    # the density passed through is piece by piece
+    # the dipole_d1 and density are passed through piece by piece
     if dm.ndim == 2:
-        vk = np.einsum('...pq,...xrs,qr->xps', dipole, dipole_d1, dm)
+        vk = np.einsum('...pq,xrs...,qr->xps', dipole, dipole_d1, dm)
         if with_j is False:
             return vk*scale_k
         else:
-            vj = np.einsum('...pq,...xrs,sr->xpq', dipole, dipole_d1, dm)
+            vj = np.einsum('...pq,xrs...,sr->xpq', dipole, dipole_d1, dm)
             return [vj, vk*scale_k]
     else: # multiple density matrices, ie uhf
-        vk = np.einsum('...pq,...xrs,iqr->ixps', dipole, dipole_d1, dm)
+        vk = np.einsum('...pq,xrs...,iqr->ixps', dipole, dipole_d1, dm)
         if with_j is False:
             return vk*scale_k
         else:
-            vj = np.einsum('...pq,...xrs,isr->ixpq', dipole, dipole_d1, dm)
+            vj = np.einsum('...pq,xrs...,isr->ixpq', dipole, dipole_d1, dm)
             return [vj, vk*scale_k]
 
 
@@ -312,16 +337,14 @@ if __name__ == '__main__':
 
     norder, step_size = 2, 1e-4
 
-    fd_e, fd_mo, fd_dip = finite_difference(mf, norder, step_size, extra=True)
-
-    print_matrix('fd_dip:', fd_dip)
+    fd_e, fd_mo = finite_difference(mf, norder, step_size, extra=True)[:2]
 
     fd_mo[0::3,5,3] = 0.
     fd_mo[np.abs(fd_mo)>2.] = 0.
     #print_matrix('fd_mo:', np.array(fd_mo), 5, 1)
 
     mo1 = fd_orbital_rotation_mo1(mf, fd_mo)
-    print_matrix('mo1', mo1, 5, 1)
+    print_matrix('fd_mo1', mo1, 5, 1)
 
     print_matrix('fd_e:', fd_e, 5, 1)
     print('hessian diff:', np.linalg.norm(fd_e-hess))

wavefunction_analysis/polariton/qed_thermo.py
@@ -9,7 +9,7 @@ from pyscf.data import nist
 from wavefunction_analysis.utils.pyscf_parser import *
 from wavefunction_analysis.utils import convert_units, print_matrix
 from wavefunction_analysis.polariton.qed_ks import polariton_cs
-from wavefunction_analysis.polariton.qed_ks_grad import get_multipole_matrix_d1
+from wavefunction_analysis.polariton.qed_ks_grad import get_multipole_matrix_d1, finite_difference
 from wavefunction_analysis.plot.vibrational_spectra import get_dipole_dev, infrared
 
 """
@@ -77,10 +77,11 @@ def get_g1_d1(mf, frequency, hessobj):
     mo_occ = mf.mo_occ
     mocc = mo_coeff[:,mo_occ>0]
 
-    dipole = mf.dipole
+    dipole = np.einsum('...pq,...->pq', mf.dipole, frequency) # combine with frequency
     dm = mf.make_rdm1()
 
     dipole_d1, _ = get_multipole_matrix_d1(mol, mf.c_lambda, mf.origin)
+    dipole_d1 = np.einsum('xpq...,...->xpq', dipole_d1, frequency)
 
     mo1 = lib.chkfile.load(hessobj.chkfile, 'scf_mo1')
     mo1 = {int(k): mo1[k] for k in mo1}
@@ -89,10 +90,10 @@ def get_g1_d1(mf, frequency, hessobj):
     aoslices = mol.aoslice_by_atom()
     for k, ia in enumerate(atmlst):
         p0, p1 = aoslices[ia, 2:]
-        dm1 = np.einsum('ypi,qi->ypq', mo1[ia], mocc)
+        dm1 = np.einsum('xpi,qi->xpq', mo1[ia], mocc)
 
-        g1[k] = np.einsum('...pq,xqp,...->x...', dipole, dm1, frequency)
-        g1[k] += np.einsum('...xpq,pq,...->x...', dipole_d1, dm, frequency)
+        g1[k] = np.einsum('pq,xqp->x', dipole, dm1)*2. # 2 for dm1
+        g1[k] += np.einsum('xpq,qp->x', dipole_d1[:,p0:p1], dm[:,p0:p1])
 
     return 2.*np.array(g1)
 
@@ -214,15 +215,19 @@ if __name__ == '__main__':
     #print_matrix('force_const_dyne:', results['force_const_dyne'])
     print_matrix('mode:', results['norm_mode'].reshape(len(results['freq_au']), -1).T)
     dip_dev = get_dipole_dev(mf, hessobj)
+    #print_matrix('dip_dev:', dip_dev, 5, 1)
     sir = infrared(dip_dev, results['norm_mode'])
     print_matrix('infrared intensity:', sir)
 
-    d1 = get_g1_d1(mf, frequency, hessobj)
-    results = harmonic_analysis(mol, [h, d1, frequency])
-    print_matrix('freq_wavenumber:', results['freq_wavenumber'])
-    #print_matrix('force_const_dyne:', results['force_const_dyne'])
-    print_matrix('mode:', results['norm_mode'].reshape(len(results['freq_au']), -1).T)
+    if np.any(np.abs(coupling) > 1e-4):
 
-    dip_dev = get_dipole_dev(mf, hessobj)
-    sir = infrared(dip_dev, results['norm_mode'])
-    print_matrix('infrared intensity:', sir)
+        d1 = get_g1_d1(mf, frequency, hessobj)
+        #print_matrix('d1:', d1, 5, 1)
+        results = harmonic_analysis(mol, [h, d1, frequency])
+        print_matrix('freq_wavenumber:', results['freq_wavenumber'])
+        #print_matrix('force_const_dyne:', results['force_const_dyne'])
+        print_matrix('mode:', results['norm_mode'].reshape(len(results['freq_au']), -1).T)
+
+        dip_dev = get_dipole_dev(mf, hessobj)
+        sir = infrared(dip_dev, results['norm_mode'])
+        print_matrix('infrared intensity:', sir)